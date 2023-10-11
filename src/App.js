f912eb0341cd2a359cbe5673f730e71de4505459
infinity29302 <146354281+infinity29302@users.noreply.github.com>
2023-10-12 01:00:46 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import About from "./About";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      new changes
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-      <About />
     </div>
   );
 }
