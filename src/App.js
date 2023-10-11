9107a282198c16884e2e5716d780d6184e6bc0d4
infinity29302 <146354281+infinity29302@users.noreply.github.com>
2023-10-12 01:21:02 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import About from "./About";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./pages/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
